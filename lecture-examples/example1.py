af66c18fb884ca4f383e4d0dd299e6dbb9b67c59
bekosha12 <be_sakipov@kbtu.kz>
2024-02-15 12:49:54 +0600

    Some lecture examples

---
+++ lecture-examples/example1.py
@@ -0,0 +1,20 @@
+# name = input()
+# print(f"Hello {name}!!!")
+
+# z = 1
+# y = 1.5
+# x = (1 + 2j)
+# print(z + y + x)
+
+str1 = '''gggg'''
+char1 = 'c'
+print(type(str1), type(char1))
+
+str2 = '''
+Some
+random
+string
+sequence
+'''
+
+print(str2)
