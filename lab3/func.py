18aaaa0fe2075bba325ae14dc70a845afdd894d4
bekosha12 <be_sakipov@kbtu.kz>
2024-03-06 22:23:15 +0500

    lab3

---
+++ lab3/func.py
@@ -0,0 +1,6 @@
+from func1 import(grams_to_ounces, fahrenheit_to_celsius, solve, filter_prime, string_permutations, reverse_words, has_33, spy_game, sphere_volume, unique_elements, is_palindrome, histogram, guess_the_number)
+grams = 11
+print(grams_to_ounces(grams))
+
+input_list = [1, 3, 0, 0, 7, 2]
+print(spy_game(input_list))
