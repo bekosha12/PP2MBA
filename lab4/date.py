5f299569717e0cae5345e50299b51b648cab4074
bekosha12 <be_sakipov@kbtu.kz>
2024-03-21 15:57:14 +0500

    lab4

---
+++ lab4/date.py
@@ -0,0 +1,26 @@
+import datetime as dt
+from datetime import datetime
+
+#ex1
+now = datetime.datetime.now()
+ans = now - datetime.timedelta(5)
+print(ans)
+
+#ex2
+now = dt.datetime.now()
+yest = now - dt.timedelta(1)
+tomor = now + dt.timedelta(1)
+print("Yesterday was ", yest)
+print("Today is ", now)
+print("Tomorrow will be ", tomor)
+
+#ex3
+now = datetime.datetime.now().replace(microsecond=0)
+print(now)
+
+#ex4
+date1 = datetime(2023, 5, 15, 12, 30, 0)  
+date2 = datetime(2023, 5, 10, 8, 45, 0)   
+minus = date1 - date2
+diff = minus.total_seconds()
+print(diff)
